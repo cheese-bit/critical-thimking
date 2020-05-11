28234e315a6e9f9eacd81038957e9c038221877b
cheese-bit <63467622+cheese-bit@users.noreply.github.com>
2020-05-10 18:43:46 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+
+namespace Critical_Thinking_Loops
+{
+    class Program
+    {
+        string numbers = "Counter";
+        for (int index = 9; index< number.length; Index++ )
+       
+                                      
+        {
+            Console.WriteLine(index);
+        }
+    }
+}
